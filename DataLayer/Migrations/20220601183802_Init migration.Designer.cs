8c80e4d51b0c7e6e09bc670a50db71e14645923b
Franklyn Brea <franklynbrea38@gmail.com>
2022-06-01 14:51:36 -0400

    Add remote Data Base

--- DataLayer/Migrations/20220601183802_Init migration.Designer.cs
+++ DataLayer/Migrations/20220601183802_Init migration.Designer.cs
@@ -3,14 +3,16 @@ using DataLayer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220601183802_Init migration")]
+    partial class Initmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataLayer/Migrations/20220601183802_Init migration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class Initmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pokemones_TipoPokemones_IdHabilidadSecundaria",
+                table: "Pokemones");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pokemones_TipoPokemones_IdHabilidadSecundaria",
+                table: "Pokemones",
+                column: "IdHabilidadSecundaria",
+                principalTable: "TipoPokemones",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pokemones_TipoPokemones_IdHabilidadSecundaria",
+                table: "Pokemones");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pokemones_TipoPokemones_IdHabilidadSecundaria",
+                table: "Pokemones",
+                column: "IdHabilidadSecundaria",
+                principalTable: "TipoPokemones",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
